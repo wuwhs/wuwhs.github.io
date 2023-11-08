e14e9bfca16c35a55312da89fa005e23a61a1ce2
吴文华(WenhuaWu)-顺丰科技技术集团 <wenhuawu@sf-express.com>
2023-11-08 16:07:13 +0800

    Site updated: 2023-11-08 16:07:12

--- demo/commonjs/a.js
+++
@@ -1,6 +0,0 @@
-const getMes = require('./b')
-console.log('我是 a 文件')
-exports.say = function () {
-  const message = getMes()
-  console.log(message)
-}
--- demo/commonjs/main.js
+++
@@ -1,7 +0,0 @@
-const a = require('./a')
-const b = require('./b')
-
-console.log('node 入口文件')
-setTimeout(() => {
-  console.log('b.author: ', b())
-}, 1000)
